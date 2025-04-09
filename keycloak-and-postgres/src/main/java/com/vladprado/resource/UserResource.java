81fd85709ad4aa49e5a34e49d1f604cd5b7dbca8
vladprado <vladprado.br@gmail.com>
2025-04-09 20:20:12 -0300

    UserResource tweak

--- keycloak-and-postgres/src/main/java/com/vladprado/resource/UserResource.java
+++ keycloak-and-postgres/src/main/java/com/vladprado/resource/UserResource.java
@@ -1,12 +1,14 @@
 package com.vladprado.resource;
 
 import io.quarkus.security.identity.SecurityIdentity;
+import jakarta.annotation.security.PermitAll;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import org.jboss.resteasy.reactive.NoCache;
 
 @Path("/api/users")
+@PermitAll
 public class UserResource {
 
     @Inject
