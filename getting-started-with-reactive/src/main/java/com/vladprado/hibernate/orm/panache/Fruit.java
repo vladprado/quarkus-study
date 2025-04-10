4bb822f56e9d7e89e751842c909fe43dd6c5a020
vladprado <vladprado.br@gmail.com>
2025-04-10 14:22:25 -0300

    Some Reactive Knowledge

---
+++ getting-started-with-reactive/src/main/java/com/vladprado/hibernate/orm/panache/Fruit.java
@@ -0,0 +1,13 @@
+package com.vladprado.hibernate.orm.panache;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+import jakarta.persistence.Cacheable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+@Cacheable
+public class Fruit extends PanacheEntity {
+    @Column(length = 40, unique = true)
+    public String name;
+}
